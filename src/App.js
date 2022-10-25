a064cc709ddae5ef346e8630aee95186003ef68e
laboneeaktar <labonipub@gmail.com>
2022-10-25 12:02:48 +0600

    Main and Routes Components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { routes } from "./Routes/Routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <h1>From h1</h1>
-      <p>from paragraph</p>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/FAQ/FAQ.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FAQ = () => {
+  return (
+    <div>
+      <h1>This is Freequently Ask Question Page</h1>
+    </div>
+  );
+};
+
+export default FAQ;
