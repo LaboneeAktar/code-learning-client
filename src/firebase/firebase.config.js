73c64f7a6ea302e29218739c0fe7a1d20c821b39
laboneeaktar <labonipub@gmail.com>
2022-10-25 11:34:05 +0600

    add title name and icon

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPSngAW1MbI5dXtnc2PwVsTJQ_McqMzqY",
+  authDomain: "learn-coding-419dc.firebaseapp.com",
+  projectId: "learn-coding-419dc",
+  storageBucket: "learn-coding-419dc.appspot.com",
+  messagingSenderId: "994378822045",
+  appId: "1:994378822045:web:3aed0418cc7ff26a877f74",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
